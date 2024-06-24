13252933cb36ec99ea4b70285262cd93425cfff6
Alexy Cruz <ACruz@uvltd.com>
2024-06-23 19:35:20 -0500

    tailwind.ts config file removed

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,13 +1,7 @@
 // postcss.config.mjs
 
 import { defineConfig } from 'postcss';
-import flexbugs from 'postcss-flexbugs-fixes';
-import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
-  plugins: [
-    flexbugs,
-    tailwindcss,
-    // Add other PostCSS plugins here if needed
-  ],
+  plugins: [ 'tailwindcss', 'autoprefixer', ],
 });
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,27 @@ const config: Config = {
   mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {
       colors: {
         'soft-blue': '#85d7ff',
         'soft-green': '#8cffa8',
       },
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    spacing: {
+      '8xl': '96rem',
+      '9xl': '128rem',
+    },
+    borderRadius: {
+      '4xl': '2rem',
     },
   },
   variants: {
